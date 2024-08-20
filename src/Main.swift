70dcfa42e990b6bd4d92e78db61819d3e12428bb
erics118 <52634785+erics118@users.noreply.github.com>
2024-08-20 00:37:20 -0400

    feat: json print option

--- src/Main.swift
+++ src/Main.swift
@@ -1,5 +1,9 @@
 import AppKit
 
+struct Browser: Codable {
+    let name, path, bundleIdentifier: String
+}
+
 func bundleIdentifier(for url: URL) -> String? {
     guard let appURL = NSWorkspace.shared.urlForApplication(toOpen: url),
           let bundle = Bundle(url: appURL)
@@ -7,6 +11,16 @@ func bundleIdentifier(for url: URL) -> String? {
         return nil
     }
     return bundle.bundleIdentifier
+}
+
+func getAppInfo(bundleIdentifier: String) -> Browser? {
+    let workspace = NSWorkspace.shared
+    if let appURL = workspace.urlForApplication(withBundleIdentifier: bundleIdentifier) {
+        let appPath = appURL.path
+        let appName = Bundle(url: appURL)?.infoDictionary?["CFBundleName"] as? String ?? ""
+        return Browser(name: appName, path: appPath, bundleIdentifier: bundleIdentifier)
+    }
+    return nil
 }
 
 func getDefaultBrowser() -> String? {
@@ -17,14 +31,19 @@ func getDefaultBrowser() -> String? {
     return bundleIdentifier(for: appURL)
 }
 
-func setDefaultBrowser(_ id: String) {
-    LSSetDefaultHandlerForURLScheme("http" as CFString, id as CFString)
+func setDefaultBrowser(_ bundleIdentifier: String) {
+    LSSetDefaultHandlerForURLScheme("http" as CFString, bundleIdentifier as CFString)
 }
 
-func getBrowsers() -> [String] {
+func getBrowsers() -> [Browser] {
     let httpURL = URL(string: "http://")!
     let urls = LSCopyApplicationURLsForURL(httpURL as CFURL, .all)?.takeRetainedValue() as? [URL] ?? []
-    return urls.compactMap { bundleIdentifier(for: $0) }
+    return urls.compactMap {
+        if let bundleIdentifier = bundleIdentifier(for: $0) {
+            return getAppInfo(bundleIdentifier: bundleIdentifier)
+        }
+        return nil
+    }
 }
 
 func printHelp() {
@@ -34,6 +53,7 @@ func printHelp() {
     Options:
         -h, --help                   Print this help message
         -l, --list                   List the available browsers
+        -j, --json                   List the available browsers as JSON
         -s, --set <bundle_id>        Set the default browser to the given bundle ID
         -g, --get                    Get the default browser
     """)
@@ -54,21 +74,31 @@ case "-l", "--list":
         print("No browsers found.")
     } else {
         for browser in browsers {
-            print(browser)
+            print(browser.bundleIdentifier)
         }
     }
 
+case "-j", "--json":
+    let browsers = getBrowsers()
+    if browsers.isEmpty {
+        print("No browsers found.")
+    } else {
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(browsers)
+        print(String(data: data, encoding: .utf8)!)
+    }
+
 case "-s", "--set":
-    guard let bundleID = args.dropFirst().first else {
+    guard let bundleIdentifier = args.dropFirst().first else {
         print("Error: missing bundle id argument")
         printHelp()
         exit(1)
     }
-    if getDefaultBrowser() == bundleID {
-        print("Browser is already set to \(bundleID)")
+    if getDefaultBrowser() == bundleIdentifier {
+        print("Browser is already set to \(bundleIdentifier)")
     } else {
-        setDefaultBrowser(bundleID)
-        print("Set default browser to \(bundleID)")
+        setDefaultBrowser(bundleIdentifier)
+        print("Set default browser to \(bundleIdentifier)")
     }
 
 case "-g", "--get":
